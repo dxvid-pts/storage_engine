4a64072d6c70d9ddcc547ac7132a929bfd2dd2a3
david.peters <david.peters@mway.io>
2023-02-02 14:57:05 +0100

    add watch function

--- storage_engine/lib/box_adapter.dart
+++ storage_engine/lib/box_adapter.dart
@@ -1,7 +1,12 @@
+import 'package:storage_engine/update_enum.dart';
+
+typedef UpdateCallback<T> = void Function(String key, UpdateAction action);
+
 abstract class BoxAdapter<T> {
   BoxAdapter();
-  
+
   late final String boxKey;
+  final List<UpdateCallback> _listeners = [];
 
   Future<void> init(String boxKey);
 
@@ -20,4 +25,14 @@ abstract class BoxAdapter<T> {
   List<T> getValues();
 
   List<String> getKeys();
+
+  void notifyListeners(String key, UpdateAction action) {
+    for (var listener in _listeners) {
+      listener(key, action);
+    }
+  }
+
+  void watch(UpdateCallback callback) {
+    _listeners.add(callback);
+  }
 }
