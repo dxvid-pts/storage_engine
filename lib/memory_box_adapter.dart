2cfa3fb77884c09d4860a5c22fa6cc6e47f42a51
david.peters <david.peters@mway.io>
2023-02-02 11:11:00 +0100

    add memory adapter

---
+++ lib/memory_box_adapter.dart
@@ -0,0 +1,28 @@
+import 'package:storage_engine/box_adapter.dart';
+
+class MemoryBoxAdapter<T> extends BoxAdapter<T> {
+  MemoryBoxAdapter();
+
+  final Map<String, T> _items = {};
+
+  @override
+  T? get(String key) => _items[key]!;
+
+  @override
+  void clear() => _items.clear();
+
+  @override
+  List<T> getValues() => _items.values.toList();
+
+  @override
+  List<String> getKeys() => _items.keys.toList();
+
+  @override
+  void remove(String key) => _items.remove(key);
+
+  @override
+  void put(String key, T value) => _items[key] = value;
+  
+  @override
+  bool containsKey(String key) => _items.containsKey(key);
+}
