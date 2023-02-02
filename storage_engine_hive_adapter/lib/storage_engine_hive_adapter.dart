f83098a3624e4fb0d33e59ec7a7b72367930e22e
david.peters <david.peters@mway.io>
2023-02-02 13:49:08 +0100

    add hive adapter

--- storage_engine_hive_adapter/lib/storage_engine_hive_adapter.dart
+++ storage_engine_hive_adapter/lib/storage_engine_hive_adapter.dart
@@ -1,7 +1,39 @@
 library storage_engine_hive_adapter;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:hive/hive.dart';
+import 'package:storage_engine/box_adapter.dart';
+
+class HiveBoxAdapter<T> extends BoxAdapter<T> {
+  HiveBoxAdapter();
+
+  @override
+  Future<void> init(String boxKey) async {
+    _box = await Hive.openBox<T>(boxKey);
+  }
+
+  late final Box<T> _box;
+
+  @override
+  T? get(String key) => _box.get(key);
+
+  @override
+  void clear() => _box.clear();
+
+  @override
+  List<T> getValues() => _box.values.toList();
+
+  @override
+  List<String> getKeys() => [..._box.keys];
+
+  @override
+  void remove(String key) => _box.delete(key);
+
+  @override
+  void put(String key, T value) => _box.put(key, value);
+
+  @override
+  bool containsKey(String key) => _box.containsKey(key);
+
+  @override
+  void putAll(Map<String, T> values) => _box.putAll(values);
 }
