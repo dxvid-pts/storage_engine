a640b418b9d6fa6e15cc5424c666e5597317eb3e
david.peters <david.peters@mway.io>
2023-02-02 13:37:25 +0100

    add migration tool

--- storage_engine/lib/storage_engine.dart
+++ storage_engine/lib/storage_engine.dart
@@ -2,20 +2,64 @@ library storage_engine;
 
 import 'package:flutter/foundation.dart';
 import 'package:storage_engine/box_adapter.dart';
+import 'package:storage_engine/memory_box_adapter.dart';
+
+const String _legacyBoxKey =
+    '96utq2@9zxwP6R4ZL6tS7Fq^HCU^y&arUx5uwS^wssPLda*zaNesWW@^PSdFDvfZK%5oz';
+const String sepereator = '--6tS7Fq^HCU^y&arUx5uw--';
 
 class StorageEngine {
   static final Map<String, BoxAdapter> _boxes = {};
+  static final BoxAdapter<bool> _legacyBoxes = MemoryBoxAdapter<bool>()
+    ..init(_legacyBoxKey);
 
-  static void registerBoxAdapter(String key, BoxAdapter adapter) {
-    _boxes[key] = adapter;
+  static Future<void> registerBoxAdapter({
+    required String collectionKey,
+    required int version,
+    required BoxAdapter adapter,
+  }) async {
+    //add box with collection key to map
+    _boxes[collectionKey] = adapter;
+
+    //generate box key from collection key and version
+    final boxKey = _getBoxKey(collectionKey, version);
+
+    //log box key and mark available data to true, so we can migrate data later
+    _legacyBoxes.put(boxKey, true);
+
+    //init box with box key
+    await adapter.init(boxKey);
   }
 
-  static void migrateBoxFromAdapterIfExist(String key, BoxAdapter oldAdapter) {
-    debugPrint('migrateFromIfExist');
+  static void migrateBoxFromAdapterIfExist<T>({
+    required String collectionKey,
+    required int oldVersion,
+    required BoxAdapter<T> oldAdapter,
+  }) {
+    final boxKey = _getBoxKey(collectionKey, oldVersion);
+
+    print(_legacyBoxes.getKeys());
+
+    //check if box has data left -> migrate
+    if (_legacyBoxes.containsKey(boxKey) && _legacyBoxes.get(boxKey)! == true) {
+      //get all data from old adapter
+      final map = Map<String, T>.fromIterables(
+          oldAdapter.getKeys(), oldAdapter.getValues());
+
+      //get current box from collection key
+      final currentBox = getBox<T>(collectionKey);
+      currentBox.putAll(map);
+
+      //mark old box as migrated
+      _legacyBoxes.put(boxKey, false);
+    }
   }
 
   static BoxAdapter<T> getBox<T>(String key) {
     assert(_boxes.containsKey(key), 'Box \'$key\' does not exist');
     return _boxes[key]! as BoxAdapter<T>;
   }
+
+  static String _getBoxKey(String key, int version) =>
+      "$key$sepereator$version";
 }
