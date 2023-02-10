126ba185a38ea3a2adc3ca4feded6760a7bd3676
david.peters <david.peters@mway.io>
2023-02-10 17:14:31 +0100

    test get keys

--- storage_engine/test/storage_engine_test.dart
+++ storage_engine/test/storage_engine_test.dart
@@ -13,23 +13,26 @@ void main() async {
 
   final box = StorageEngine.getBox<String>(testCollectionKey);
 
+  const key = "testKey";
+  const value = "testValue";
+
+  const key2 = "testKey2";
+  const value2 = "testValue2";
+
   //write tests for put
   test('box put', () async {
-    //-------------test put: key = val -> get key => val -------------
-    const key = "testKey";
-    const value = "testValue";
+    //clear box for next tests
+    await box.clear();
 
+    //-------------test put: key = val -> get key => val -------------
     await box.put(key, value);
     expect(await box.get(key), value);
 
     //-------------test put: key2 = val ->  get key => != val2 -------------
-    const key2 = "testKey2";
-    const value2 = "testValue2";
-
     await box.put(key2, value);
     expect(await box.get(key2), isNot(equals(value2)));
 
-    //-------------test put: key2 = val2 ->  get key2 => val2 ------------- 
+    //-------------test put: key2 = val2 ->  get key2 => val2 -------------
     //(test if put overwrites)
     await box.put(key2, value2);
     expect(await box.get(key2), value2);
@@ -37,14 +40,26 @@ void main() async {
 
   //write tests for containsKey
   test('box containsKey', () async {
-    //-------------test containsKey: key = val -> containsKey key => true -------------
-    const key = "testKey";
-    const value = "testValue";
+    //clear box for next tests
+    await box.clear();
 
+    //-------------test containsKey: key = val -> containsKey key => true -------------
     await box.put(key, value);
     expect(await box.containsKey(key), true);
 
     //-------------test containsKey:  containsKey randomKey => false -------------
     expect(await box.containsKey("aefjbglisbgabegosbgo"), false);
+  });
+
+  //write tests for getKeys
+  test('box getKeys', () async {
+    //clear box for next tests
+    await box.clear();
+
+    //-------------test getKeys: key = val -> getKeys => [key] -------------
+    await box.put(key, value);
+    await box.put(key2, value2);
+
+    expect(await box.getKeys(), [key, key2]);
   });
 }
