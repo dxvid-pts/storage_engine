99682c0ac9550f26e72fa1ddeb7621b1a69d3ca8
david.peters <david.peters@mway.io>
2023-02-10 17:09:31 +0100

    test put and contains

--- storage_engine/test/storage_engine_test.dart
+++ storage_engine/test/storage_engine_test.dart
@@ -3,24 +3,48 @@ import 'package:storage_engine/memory_box_adapter.dart';
 
 import 'package:storage_engine/storage_engine.dart';
 
-void main() {
-  test('adds one to input values', () {
-    // const adapter = MemoryAdapter(id: 'memory-v1');
-    //StorageEngine.registerAdapter(adapter);
+void main() async {
+  const testCollectionKey = "testBox";
+  await StorageEngine.registerBoxAdapter<String>(
+    collectionKey: testCollectionKey,
+    version: 1,
+    adapter: MemoryBoxAdapter(),
+  );
 
-    // adapt<SampleClass>((object) => [object.name, object.age, object.height]);
+  final box = StorageEngine.getBox<String>(testCollectionKey);
 
-    //general adapter -> class to map
-    //specific adapter -> map to db
-    //example: memory adapter -> for each key value pair
-    StorageEngine.registerBoxAdapter('ingredients', MemoryBoxAdapter<String>());
+  //write tests for put
+  test('box put', () async {
+    //-------------test put: key = val -> get key => val -------------
+    const key = "testKey";
+    const value = "testValue";
 
-    final box = StorageEngine.getBox<String>('ingredients');
-    box.put('key', 'value');
-    print(box.get("key"));
-    /* final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);*/
+    await box.put(key, value);
+    expect(await box.get(key), value);
+
+    //-------------test put: key2 = val ->  get key => != val2 -------------
+    const key2 = "testKey2";
+    const value2 = "testValue2";
+
+    await box.put(key2, value);
+    expect(await box.get(key2), isNot(equals(value2)));
+
+    //-------------test put: key2 = val2 ->  get key2 => val2 ------------- 
+    //(test if put overwrites)
+    await box.put(key2, value2);
+    expect(await box.get(key2), value2);
+  });
+
+  //write tests for containsKey
+  test('box containsKey', () async {
+    //-------------test containsKey: key = val -> containsKey key => true -------------
+    const key = "testKey";
+    const value = "testValue";
+
+    await box.put(key, value);
+    expect(await box.containsKey(key), true);
+
+    //-------------test containsKey:  containsKey randomKey => false -------------
+    expect(await box.containsKey("aefjbglisbgabegosbgo"), false);
   });
 }
